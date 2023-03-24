3b79bb5fa81f9241f18fa22b9cf0934511a66c61
Avinash Yadav <65644572+Avinash0505@users.noreply.github.com>
2023-03-24 19:49:01 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ### In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1) Python
+# 2) SQL
+# 3) Ruby
+# 4) Java
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1) Tensorflow
+# 2) Pandas
+# 3) Scipy
+# 4) Pandas
+
+# In[5]:
+
+
+import pandas as pd
+
+
+# In[9]:
+
+
+data={'Data Science Tools': ['Anaconda','Jupyter','Pycharm','VS Code']}
+df=pd.DataFrame(data)
+print(df)
+
+
+# In[1]:
+
+
+#This a simple arithmetic expression to mutiply then add integers 
+print((3*4)+5)
+
+
+# In[2]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+min=200
+hour=200/60
+print(hour)
+
+
+# **Objectives**
+# 
+
+# * java
+# * python
+# * sql
+# * javascript
+
+# ## Author
+
+# Avinash Yadav
+
+# In[ ]:
+
+
+
+
